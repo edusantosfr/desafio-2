12b1a217007334bcd60fc49896b77513ae087947
edusantosfr <70604190+edusantosfr@users.noreply.github.com>
2025-02-22 17:02:53 -0300

    adding small details to profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -29,8 +29,8 @@ export function Profile() {
                         </div>
                         <div className="flex flex-col gap-10 pb-2">
                             <h1 className="text-[30px] font-bold text-[#303030]">Repositórios</h1>
-                            <div>
-                                <div className="w-[365px] h-[280px] rounded-[10px] shadow-[0_0_20px_rgba(0,0,0,0.10)] grid grid-rows-[70px_210px]">
+                            <div className="flex gap-8">
+                                <div className="w-[340px] h-[280px] rounded-[10px] shadow-[0_0_20px_rgba(0,0,0,0.20)] grid grid-rows-[70px_210px]">
                                     <div className="flex items-center justify-center border-b-1 border-[#0070E0]">
                                         <div className="w-[80%]">
                                             <h3 className="flex font-bold text-[#202E49]">Repositório</h3>
@@ -49,8 +49,44 @@ export function Profile() {
                                         </div>
                                     </div>
                                 </div>
-                                <div></div>
-                                <div></div>
+                                <div className="w-[365px] h-[280px] rounded-[10px] shadow-[0_0_20px_rgba(0,0,0,0.20)] grid grid-rows-[70px_210px]">
+                                    <div className="flex items-center justify-center border-b-1 border-[#0070E0]">
+                                        <div className="w-[80%]">
+                                            <h3 className="flex font-bold text-[#202E49]">Repositório</h3>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center justify-center">
+                                        <div className="w-[80%] flex flex-col gap-5">
+                                            <div className="bg-[#F7F7F7] w-[100%] h-[60px] rounded-[11px] flex flex-col justify-center">
+                                                <span className="font-normal text-[12px] text-[#6A6F73] pl-5">Link</span>
+                                                <p className="font-medium text-[14px] text-[#202E49] underline pl-5">www.link.com.br</p>
+                                            </div>
+                                            <div className="bg-[#F7F7F7] w-[100%] h-[60px] rounded-[11px] flex flex-col justify-center">
+                                                <span className="font-normal text-[12px] text-[#6A6F73] pl-5">Link</span>
+                                                <p className="font-medium text-[14px] text-[#202E49] pl-5">Lorem ipsum dolor...</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="w-[365px] h-[280px] rounded-[10px] shadow-[0_0_20px_rgba(0,0,0,0.20)] grid grid-rows-[70px_210px]">
+                                    <div className="flex items-center justify-center border-b-1 border-[#0070E0]">
+                                        <div className="w-[80%]">
+                                            <h3 className="flex font-bold text-[#202E49]">Repositório</h3>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center justify-center">
+                                        <div className="w-[80%] flex flex-col gap-5">
+                                            <div className="bg-[#F7F7F7] w-[100%] h-[60px] rounded-[11px] flex flex-col justify-center">
+                                                <span className="font-normal text-[12px] text-[#6A6F73] pl-5">Link</span>
+                                                <p className="font-medium text-[14px] text-[#202E49] underline pl-5">www.link.com.br</p>
+                                            </div>
+                                            <div className="bg-[#F7F7F7] w-[100%] h-[60px] rounded-[11px] flex flex-col justify-center">
+                                                <span className="font-normal text-[12px] text-[#6A6F73] pl-5">Link</span>
+                                                <p className="font-medium text-[14px] text-[#202E49] pl-5">Lorem ipsum dolor...</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
