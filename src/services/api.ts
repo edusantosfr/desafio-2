cf2103490c3d5ccb2abd2734f221f9cfd4fa0e88
edusantosfr <70604190+edusantosfr@users.noreply.github.com>
2025-03-04 18:03:21 -0300

    api fixing

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const API = axios.create({
   baseURL: "https://api.github.com/users",
   headers: {
-    'Authorization': `token`,
     "Content-Type": "application/json",
     Accept: "application/json",
   },
